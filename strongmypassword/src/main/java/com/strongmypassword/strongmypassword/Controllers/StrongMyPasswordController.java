08d9ced61a4de08ac6b91790bc9efba94ff17343
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-07-23 17:54:32 -0300

    seed end-point

--- strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Controllers/StrongMyPasswordController.java
+++ strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Controllers/StrongMyPasswordController.java
@@ -1,15 +1,24 @@
 package com.strongmypassword.strongmypassword.Controllers;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.strongmypassword.strongmypassword.dto.PasswordDTO;
+import com.strongmypassword.strongmypassword.dto.SeedsDTO;
 
 @RestController
 public class StrongMyPasswordController {
 
     @GetMapping("/strongme")
-    public String StrongMyPassword(@RequestBody String password){
-        String receved_password = "senha recebida = " + password;
-        return receved_password;
+        public ResponseEntity<String>StrongMe(@RequestBody PasswordDTO password){
+        String receved_password = "senha recebida = " + password.password();
+        return ResponseEntity.ok().body(receved_password);
     }
+
+    @GetMapping("/seedmypassword")
+    public ResponseEntity<String> SeedMyPassword(@RequestBody SeedsDTO seeds){
+        String password = seeds.seeds().get(1);
+        return ResponseEntity.ok().body(password);
+    }
+    
 }
