cfcf8ccd9b0bbcc80dd1dafc4551a916e8d6d541
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-07-24 16:13:10 -0300

    DTO with parameters

--- strongmypassword/src/main/java/com/strongmypassword/strongmypassword/dto/PasswordDTO.java
+++ strongmypassword/src/main/java/com/strongmypassword/strongmypassword/dto/PasswordDTO.java
@@ -1,5 +1,5 @@
 package com.strongmypassword.strongmypassword.dto;
 
-public record PasswordDTO(String password) {
+public record PasswordDTO(String password,int minLength,boolean requireUppercase,boolean requireNumber,boolean requireSpecialChar) {
 
 }
