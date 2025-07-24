ae2cf5d4ce9abb3f98e9e44fc76a73bf7cbcfbb1
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-07-24 16:12:54 -0300

    process parameters

--- strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Services/StrongMyPasswordService.java
+++ strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Services/StrongMyPasswordService.java
@@ -1,22 +1,48 @@
 package com.strongmypassword.strongmypassword.Services;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.strongmypassword.strongmypassword.Util.PasswordStrengthner;
+import com.strongmypassword.strongmypassword.Util.PasswordValidator;
+import com.strongmypassword.strongmypassword.dto.PasswordDTO;
 
 
 @Service
 public class StrongMyPasswordService {
     @Autowired
     PasswordStrengthner passwordStrengthner;
+    @Autowired
+    PasswordValidator passwordValidator;
     
-    public String Strengthner(String WeakPassword){
-        String StrogerPassword = passwordStrengthner.ShuffleNumbersSequence(WeakPassword);
+    public String Strengthner(PasswordDTO WeakPassword){
+        String StrogerPassword = WeakPassword.password();
+
+        if (!passwordValidator.VerifySize(WeakPassword.password(),WeakPassword.minLength())) {
+            StrogerPassword = passwordStrengthner.MinLength(StrogerPassword,WeakPassword.minLength());
+        }
+
+        StrogerPassword = passwordStrengthner.ShuffleNumbersSequence(StrogerPassword);
         StrogerPassword = passwordStrengthner.ChangeCharacters(StrogerPassword);
-        return StrogerPassword;
+
+        if (WeakPassword.requireNumber()) {
+            if (!passwordValidator.VerifyNumbers(StrogerPassword)) {
+              StrogerPassword = passwordStrengthner.MinOneNumber(StrogerPassword);
+            }
+        }
+        if (WeakPassword.requireSpecialChar()) {
+            if (!passwordValidator.VerifyEspecialCharacter(StrogerPassword)) {
+                while (!passwordValidator.VerifyEspecialCharacter(StrogerPassword)) {
+                    StrogerPassword = passwordStrengthner.ChangeCharacters(StrogerPassword);
+                }
+            }
+        }
+        if (WeakPassword.requireUppercase()) {
+            if (!passwordValidator.VerifyUper(StrogerPassword)) {
+               StrogerPassword = passwordStrengthner.MinOneUperCharacter(StrogerPassword);
+            }
+        }
+       
+    return StrogerPassword;
+
     }
 
 }
