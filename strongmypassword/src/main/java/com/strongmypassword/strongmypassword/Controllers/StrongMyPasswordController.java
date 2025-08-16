3460f758c6266eee8cb8dd8f32dcb4b1097c5a92
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-08-16 19:59:42 -0300

    POST

--- strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Controllers/StrongMyPasswordController.java
+++ strongmypassword/src/main/java/com/strongmypassword/strongmypassword/Controllers/StrongMyPasswordController.java
@@ -2,6 +2,7 @@ package com.strongmypassword.strongmypassword.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +15,15 @@ public class StrongMyPasswordController {
 
     @Autowired
     StrongMyPasswordService strongMyPasswordService;
-    @GetMapping("/strongme")
+    @PostMapping("/strongme")
         public ResponseEntity<String>StrongMe(@RequestBody PasswordDTO password){
         
         String newPassword = strongMyPasswordService.Strengthner(password);
+        System.out.println(newPassword);
         return ResponseEntity.ok().body(newPassword);
     }
 
-    @GetMapping("/seedmypassword")
+    @PostMapping("/seedmypassword")
     public ResponseEntity<String> SeedMyPassword(@RequestBody SeedsDTO seeds){
         String password = strongMyPasswordService.HashFromSeeds(seeds);
         return ResponseEntity.ok().body(password);
